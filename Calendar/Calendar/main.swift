e072acf6e3ddc3a1939b9da3419c94948e78613b
Russell Gordon <rgordon@rsgc.on.ca>
2016-03-06 09:25:58 -0500

    Created empty command line application

---
+++ Calendar/Calendar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calendar
+//
+//  Created by Russell Gordon on 2016-03-06.
+//  Copyright © 2016 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
