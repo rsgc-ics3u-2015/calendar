57ef584e4a21e10d75f417d020caafd67da85160
Russell Gordon <rgordon@rsgc.on.ca>
2016-03-06 09:07:45 -0500

    Prints empty spaces in calendar at start

--- Calendar.playground/Contents.swift
+++ Calendar.playground/Contents.swift
@@ -10,7 +10,7 @@
 // are 31 days.
 //
 // Then I'd have:
-// 
+//
 // Sun Mon Tue Wed Thr Fri Sat
 //                           1
 //   2   3   4   5   6   7   8
@@ -22,3 +22,42 @@
 // So, in this worst case scenario, I'd need 6 rows, and there are always 7 columns.
 // I need to pad the smaller numbers with spaces. There is always one space between
 // a column. And I have to watch out for the special day and place an asterisk there.
+
+// Input variables
+let startDay : Int = 7
+let daysInMonth : Int = 31
+let specialDay : Int = 2
+
+// Other variables
+var currentPositionInCalendar : Int = 1
+var currentDayInMonth : Int = 1
+
+// Process and output
+
+// Print out the header
+print("Sun Mon Tue Wed Thr Fri Sat")
+
+// Generate output
+for row in 1...6 {
+    
+    // Go across the columns for this row
+    for column in 1...7 {
+        
+        // When less than the start day, print three blank spaces
+        if currentPositionInCalendar < startDay {
+            
+            print("   ", terminator: "")
+            
+        }
+        
+        // Always print a space between columns (for now use an underscore for clarity)
+        print("_", terminator: "")
+        
+        // Increment current position in calendar
+        currentPositionInCalendar += 1
+    }
+    
+    // Move down to the next row
+    print("")
+    
+}
